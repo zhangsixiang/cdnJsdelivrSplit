c80e4be9cfab00123bc954c37fd0e61ba0ad4adf
= <=>
2021-05-01 22:49:43 +0800

    新增图片预览功能

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
   <img src="https://cdn.jsdelivr.net/gh/zhangsixiang/tqlFile@master/file1/jscdn.gif">
 </p>
 <p>这是一个简化使用cdn.Jsdelivr.com链接拼合的小工具</p>
+<p>2021/5/1：新增图片预览功能</p>
--- js/main.js
+++ js/main.js
@@ -2,41 +2,48 @@ new Vue({
     el: '#app',
     data: function () {
         return {
-            input: null,
-            input1: null,
-            userName: 'zhangsixiang',
-            repoName: 'tqlFile',
-            version: "master",
-            fileName: "file1",
-            text: null,
+            ImgName: null,  // 图片名称
+            copyText: null, // 复制的文本
+            userName: 'zhangsixiang', // 您的GitHub名字
+            repoName: 'tqlFile', // 您的仓库名字
+            version: "master", // 您的release版本
+            fileName: "file1", // 您的文件夹
+            text: null, // 最终拼合的字符串
+            src: '' // 图片src
         }
     },
-    created(){
+    watch: {
+        // 如果 `question` 发生改变，这个函数就会运行
+        ImgName: function (newQuestion, oldQuestion) {
+            this.piecen()
+        }
+    },
+    created() {
         this.text = `https://cdn.jsdelivr.net/gh/${this.userName}/${this.repoName}@${this.version}/${this.fileName}/`
     },
-    updated(){
+    updated() {
         this.text = `https://cdn.jsdelivr.net/gh/${this.userName}/${this.repoName}@${this.version}/${this.fileName}/`
-        console.log("222");
     },
     methods: {
         piecen() {
-            if (this.input) {
-                this.input1 = null
-                this.input1 = this.text + this.input
+            if (this.ImgName) {
+                this.copyText = null
+                this.copyText = this.text + this.ImgName
             } else {
-                this.input1 = '无'
+                this.copyText = '无'
             }
         },
         copy() {
-            var input = document.getElementById("input");
-            input.select(); // 选中文本
+            var ImgName = document.getElementById("copyText");
+            ImgName.select(); // 选中文本
             var obj = document.execCommand("copy");
-            if (this.input) {
+            if (this.ImgName) {
                 this.$notify({
                     title: '成功',
                     message: '复制成功',
                     type: 'success'
                 });
+                this.src = this.copyText
             } else {
                 this.$notify.error({
                     title: '错误',
