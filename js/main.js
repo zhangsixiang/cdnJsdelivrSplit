e53e99ce72f5668c87147c568bc1992c0a078b88
coding <961051447@qq.com>
2021-09-15 11:24:38 +0800

    新增【文件下载链接拼合】

--- js/main.js
+++ js/main.js
@@ -7,16 +7,38 @@ new Vue({
             userName: 'zhangsixiang', // 您的GitHub名字
             repoName: 'tqlFile', // 您的仓库名字
             version: "master", // 您的release版本
-            fileName: "file1", // 您的文件夹
+            fileName: "webp", // 您的文件夹
             text: null, // 最终拼合的字符串
-            src: '' // 图片src
+            src: '', // 图片src
+            options: [{
+                value: '蓝奏云',
+                label: '蓝奏云'
+            }, {
+                value: 'GitHub',
+                label: 'GitHub'
+            }, {
+                value: '百度网盘',
+                label: '百度网盘'
+            }, {
+                value: '磁力链接',
+                label: '磁力链接'
+            }],
+            panName: '蓝奏云', // 您的网盘名称
+            linkName: null, // 您的资源链接 
+            copyLink: null // 复制的文本
         }
     },
     watch: {
         // 如果 `question` 发生改变，这个函数就会运行
         ImgName: function (newQuestion, oldQuestion) {
             this.piecen()
-        }
+        },
+        panName: function(newQuestion, oldQuestion){
+            this.piecenLink()
+        },
+        linkName: function(newQuestion, oldQuestion){
+            this.piecenLink()
+        },
     },
     created() {
         this.text = `https://cdn.jsdelivr.net/gh/${this.userName}/${this.repoName}@${this.version}/${this.fileName}/`
@@ -31,6 +53,14 @@ new Vue({
                 this.copyText = this.text + this.ImgName
             } else {
                 this.copyText = '无'
+            }
+        },
+        piecenLink() {
+            if (this.linkName) {
+                this.copyLink = null
+                this.copyLink = `<p class="has-text-align-center">${this.panName}：<a href="${this.linkName}" target="_blank" rel="noreferrer noopener">${this.linkName}</a></p>`
+            } else {
+                this.copyLink = '无'
             }
         },
         copy() {
@@ -48,6 +78,25 @@ new Vue({
                 this.$notify.error({
                     title: '错误',
                     message: '请输入图片名称'
+                });
+            }
+
+        },
+        copyLinkF() {
+            var linkName = document.getElementById("copyLink");
+            linkName.select(); // 选中文本
+            var obj = document.execCommand("copy");
+            if (this.linkName) {
+                this.$notify({
+                    title: '成功',
+                    message: '复制成功',
+                    type: 'success'
+                });
+                this.src = this.copyLink
+            } else {
+                this.$notify.error({
+                    title: '错误',
+                    message: '请输入资源链接'
                 });
             }
 
